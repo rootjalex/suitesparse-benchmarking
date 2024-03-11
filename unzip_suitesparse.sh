6d79f7c8e06fe53a316f4f06ea47adf61f8593f2
Alexander <ajroot@stanford.edu>
2024-03-11 13:43:20 -0700

    initial commit

---
+++ unzip_suitesparse.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 360
+
+# Command: ./scripts/get_data/unpack_suitesparse_all.sh
+
+# From: https://github.com/weiya711/sam/blob/master/scripts/get_data/unpack_suitesparse.sh
+
+cd $SUITESPARSE_PATH
+
+for f in *.tar.gz; do
+    tar -xvf "$f" --strip=1
+    rm "$f"
+done
+
+for f in *.tar.gz.1; do
+    rm "$f"
+done
+
+for f in *.mtx; do
+    chmod ugo+r "$f"
+done
